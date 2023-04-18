69a3f035ea0639fc46126a447a7ba8c36c8e5516
Nagaraju1718 <131112689+Nagaraju1718@users.noreply.github.com>
2023-04-18 12:54:44 +0530

    Update addtwo.py

--- addtwo.py
+++ addtwo.py
@@ -1,3 +1,9 @@
 a=int(input("Enter an integer: ")
 b=int(input("Enter another integer: ")
-print(a+b)
+c=int(input("Enter another integer: ")
+if(a>b AND a>c):
+      print("Greatest: "+a)
+ elif(b>c AND b>a):
+      print("Greatest: "+b)
+ else:
+      print("Greatest: "+c)
